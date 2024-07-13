d19bf34423b64499ddac9d1c6f0bf2621010f35f
CarolPadilla <163451549+CarolPadilla@users.noreply.github.com>
2024-07-12 21:23:54 -0400

    cambios

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\GorillaSetups\Documents\GitHub\comics\myenv
+set VIRTUAL_ENV=C:\Users\ProBookR7\Documents\GitHub\comics\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
